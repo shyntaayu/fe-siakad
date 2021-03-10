fb110858a39a20334cae1d226e3bd97aa107da20
shyntaayu <shynta.add@gmail.com>
2021-03-10 17:44:42 +0700

    + input nilai (mockup) + khs (mockup)

--- src/app/main/khs/khs.component.ts
+++ src/app/main/khs/khs.component.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MessageService, SelectItem } from "primeng/api";
+import { MainService } from "../main.service";
+import { Product } from "../model/product";
 
 @Component({
-  selector: 'app-khs',
-  templateUrl: './khs.component.html',
-  styleUrls: ['./khs.component.css']
+  selector: "app-khs",
+  templateUrl: "./khs.component.html",
+  styleUrls: ["./khs.component.css"],
 })
 export class KhsComponent implements OnInit {
+  profileForm: FormGroup;
+  options: string[] = ["One", "Two", "Three"];
+  products: Product[];
+  selectedProduct2: Product;
+  products2: Product[];
+  clonedProducts: { [s: string]: Product } = {};
+  statuses: SelectItem[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private productService: MainService,
+    private messageService: MessageService
+  ) {
+    this.profileForm = this.fb.group({
+      jenis: ["", Validators.required],
+      semester: ["", Validators.required],
+      tahun: ["", Validators.required],
+      jurusan: ["", Validators.required],
+    });
   }
 
+  ngOnInit(): void {
+    this.productService.getProductsSmall().then((data) => {
+      this.products = data;
+      console.log(data);
+    });
+    this.productService
+      .getProductsSmall()
+      .then((data) => (this.products2 = data));
+    this.statuses = [
+      { label: "In Stock", value: "INSTOCK" },
+      { label: "Low Stock", value: "LOWSTOCK" },
+      { label: "Out of Stock", value: "OUTOFSTOCK" },
+    ];
+  }
+
+  onRowSelect(event) {
+    this.messageService.add({
+      severity: "info",
+      summary: "Product Selected",
+      detail: event.data.name,
+    });
+  }
+
+  onRowUnselect(event) {
+    this.messageService.add({
+      severity: "info",
+      summary: "Product Unselected",
+      detail: event.data.name,
+    });
+  }
+
+  onRowEditInit(product: Product) {
+    this.clonedProducts[product.id] = { ...product };
+  }
+
+  onRowEditSave(product: Product) {
+    if (product.price > 0) {
+      delete this.clonedProducts[product.id];
+      this.messageService.add({
+        severity: "success",
+        summary: "Success",
+        detail: "Product is updated",
+      });
+    } else {
+      this.messageService.add({
+        severity: "error",
+        summary: "Error",
+        detail: "Invalid Price",
+      });
+    }
+  }
+
+  onRowEditCancel(product: Product, index: number) {
+    this.products2[index] = this.clonedProducts[product.id];
+    delete this.clonedProducts[product.id];
+  }
 }
